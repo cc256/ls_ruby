263b3e107f8be310459667f65597a2b5045fcd22
cc256 <cc256xs@gmail.com>
2019-06-26 20:03:59 +0300

    change

--- RB120 - Object Oriented Programming/6 Easy 2/3.rb
+++ RB120 - Object Oriented Programming/6 Easy 2/3.rb
@@ -18,5 +18,5 @@ home1 = House.new(100_000)
 home2 = House.new(150_000)
 puts "Home 1 is cheaper" if home1 < home2             # => Home 1 is cheaper          < > ruby syntactic sugar?
 puts "Home 1 is cheaper" if home1.<(home2)            # => Home 1 is cheaper
-puts "Home 2 is more expensive" if home2.>(home1)     # => Home 2 is more expensive
+puts "Home 2 is more expensive" if home2 > home1      # => Home 2 is more expensive
 puts "Home 2 is more expensive" if home2.>(home1)     # => Home 2 is more expensive
